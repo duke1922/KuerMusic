abcadb630b3e0468dfa1b2ddab8591ba26f71a11
Duke <duke.yan@3medu.com>
2018-04-10 11:40:36 +0800

    修改名称

--- src/config/def.js
+++ src/config/def.js
@@ -11,7 +11,7 @@ export const SINGLE = 0
 export const SEQUENTIAL = 1
 export const RANDOM = 2
 export const DEFAULT_IMG = require('../assets/Vue_Music_Blur.png')
-export const DEFAULT_SONG_NAME = 'VUE MUSIC'
+export const DEFAULT_SONG_NAME = 'My MUSIC'
 export const types = {
   ALBUM: 10002,
   CD: 10014,
